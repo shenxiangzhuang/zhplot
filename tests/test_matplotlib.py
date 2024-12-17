3222de36d2ff955d064bdaccb9806a4cc71d50d4
Xiangzhuang Shen <datahonor@gmail.com>
2024-12-17 21:43:11 +0800

    fix(test): remove test

--- tests/test_matplotlib.py
+++ tests/test_matplotlib.py
@@ -3,14 +3,6 @@ import matplotlib.pyplot as plt
 import zhplot  # noqa
 
 
-def test_simple_plot():
-    """Test basic plotting with Chinese labels."""
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4])
-    ax.set_xlabel("简单测试")
-    plt.close(fig)
-
-
 def test_chinese_font_config():
     """Test if matplotlib is configured to use Chinese fonts."""
     # Check if SimHei is in the font family settings after the import
